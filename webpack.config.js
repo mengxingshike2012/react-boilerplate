d2f16590fa3c5b191801e2e0552b504e9640153a
wind_fantasy <wind_fantasy@wind-fantasydeMac.local>
2016-09-06 22:57:16 +0800

    update config

--- webpack.config.js
+++ webpack.config.js
@@ -18,28 +18,31 @@ module.exports = {
     loaders: [
       {
         test: /\.(js|jsx)$/,
-        loader: 'babel-loader',
-        include:[path.join(__dirname, 'src')]
+        include:[path.join(__dirname, 'src')],
+        loaders: ['react-hot', 'babel']
       }
     ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx'],
-    alias: {
-      actions: path.join(__dirname, 'src/actions/'),
-      components: path.join(__dirname, 'src/components/'),
-      containers: path.join(__dirname, 'src/containers'),
-      middlewares: path.join(__dirname, 'src/middlewares/'),
-      reducers: path.join(__dirname, 'src/reducers/'),
-      sagas: path.join(__dirname, 'src/sagas/'),
-      styles: path.join(__dirname, 'src/styles/'),
-    }
+    root: path.resolve('./src'),
+    // alias: {
+    //   actions: path.join(__dirname, 'src/actions/'),
+    //   components: path.join(__dirname, 'src/components/'),
+    //   containers: path.join(__dirname, 'src/containers'),
+    //   middlewares: path.join(__dirname, 'src/middlewares/'),
+    //   reducers: path.join(__dirname, 'src/reducers/'),
+    //   sagas: path.join(__dirname, 'src/sagas/'),
+    //   styles: path.join(__dirname, 'src/styles/'),
+    // }
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: 'src/index.html'
+      template: 'src/index.html',
+      cache: false
     }),
   ],
   devServer: {
+
   }
 }
