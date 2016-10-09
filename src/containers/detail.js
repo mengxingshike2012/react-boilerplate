a39bf3b1e7bc5617a8fae00063a295c73d9934cf
wangjian <wangjian@dianwoba.com>
2016-10-09 14:47:48 +0800

    add detail file

---
+++ src/containers/detail.js
@@ -0,0 +1,36 @@
+import React, {
+  PropTypes,
+} from 'react';
+import { connect } from 'react-redux';
+import { routerShape, withRouter } from 'react-router';
+import css from 'react-css-modules';
+import styles from './index.scss';
+
+import { fetchTopicDetail } from 'actions/v2ex';
+
+@connect(state => ({
+  data: state.v2ex.detail,
+}))
+@css(styles)
+export default class TopicDetail extends React.Component {
+  static propTypes = {
+    router: routerShape,
+    params: PropTypes.object,
+    data: PropTypes.object,
+  }
+  componentDidMount() {
+    const id = this.props.params.id;
+    this.props.dispatch(fetchTopicDetail(id));
+  }
+
+  render() {
+    const { data } = this.props;
+    if (!data) return <div>Not Matched!</div>;
+    return (
+      <div styleName="main">
+        <div styleName="title">{data.title}</div>
+        <div styleName="detail">{data.content}</div>
+      </div>
+    );
+  }
+}
