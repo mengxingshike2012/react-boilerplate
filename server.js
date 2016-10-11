e0b65a8793534d192445b4ff23713dd48af79351
wangjian <wangjian@dianwoba.com>
2016-10-11 11:45:26 +0800

    miss: git commit -am will not add new files

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const fetch = require('isomorphic-fetch');
+
+const app = express();
+const api = express.Router();
+const hostname = 'https://www.v2ex.com';
+const port = 8001;
+api
+  .get('/topics/hot.json', (req, res) => {
+    const path = req.originalUrl.replace('/\/api/', '');
+    const url = hostname + path;
+    console.log(url);
+    fetch(url).then((data) => {
+      return data.json();
+    }).then((json) => {
+      res.send(json);
+    }).catch((e) => {
+      console.log(e);
+    });
+  })
+  .get('/topics/:id', (req, res) => {
+    const url = `${hostname}/api/topics/show.json?id=${req.params.id}`;
+    console.log(url);
+    fetch(url).then((data) => {
+      return data.json();
+    }).then((json) => {
+      res.send(json);
+    }).catch((e) => {
+      console.log(e);
+    });
+  });
+
+app.use(cors());
+app.use('/api', api);
+
+app.listen(port, () => {
+  console.log(`server has been listening on port:${port}`);
+});
